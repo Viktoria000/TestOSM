779bdd926a8da9b069df4d678fc8388d61406df4
Селькова <selkova@protei.ru>
2021-08-12 18:55:53 +0300

    Исправления allure

--- test_direct_search.py
+++ test_direct_search.py
@@ -24,52 +24,58 @@ class Coordinates:
 # объявляем функцию, добавляем параметр в словарь, отправляем запрос, ответ в формате джейсон
 def direct_search(req_params):
     req_params["format"] = "json"
-    response = requests.get("https://nominatim.openstreetmap.org/search?", params=req_params)
-    json_response = response.json()
-    result = []
-    for json_item in json_response:
-        display_name = json_item["display_name"]
-        lat = json_item["lat"]
-        lon = json_item["lon"]
-        coords = Coordinates(display_name, lat, lon)
-        result.append(coords)
-
+    with allure.step("Отправляем запрос"):
+        response = requests.get("https://nominatim.openstreetmap.org/search?", params=req_params)
+        json_response = response.json()
+        result = []
+        with allure.step("Создаем цикл, который проходит по объектам ответа"):
+            for json_item in json_response:
+                display_name = json_item["display_name"]
+                lat = json_item["lat"]
+                lon = json_item["lon"]
+                with allure.step("Создаем объект класса и добавляем в конец строки)"):
+                    coords = Coordinates(display_name, lat, lon)
+                    result.append(coords)
     return result
 
 
 # объявляем тест с параметрами (два набора входных данных)
 @pytest.mark.parametrize("param_list", direct_param_list_value)
 def test_direct_any_params(param_list):
-    req_params = param_list["req_params"]
-    coordinates_list = direct_search(req_params)
+    with allure.step("Объявляем переменную принимающую заданные параметры"):
+        req_params = param_list["req_params"]
+        coordinates_list = direct_search(req_params)
 
-    expected_lat = param_list["expected_lat"]
-    expected_lon = param_list["expected_lon"]
+        expected_lat = param_list["expected_lat"]
+        expected_lon = param_list["expected_lon"]
 
     find_coordinates = False
-    for coordinates in coordinates_list:
-        if coordinates.lat == expected_lat and coordinates.lon == expected_lon:
-            find_coordinates = True
-            print("FOUND!!")
-            coordinates.print()
-
-    assert find_coordinates, "not find expected coordinates"
+    with allure.step("Создаем цикл по объектам класса"):
+        for coordinates in coordinates_list:
+            with allure.step("Сравниваем параметры объекта класса с заданными параметрами"):
+                if coordinates.lat == expected_lat and coordinates.lon == expected_lon:
+                    find_coordinates = True
+                    print("FOUND!!")
+                    coordinates.print()
+                    assert find_coordinates, "not find expected coordinates"
 
 
 @pytest.mark.parametrize("reverse_param", reverse_param_list_value)
 def test_revers_param(reverse_param):
-    lat = reverse_param["coordinats"]["lat"]
-    lon = reverse_param["coordinats"]["lon"]
-    dict_requests = {"lat": lat, "lon": lon, "format": "json", "accept-language": "ru"}
-    response = requests.get("https://nominatim.openstreetmap.org/reverse?", params=dict_requests)
-    json_response = response.json()
-    expected_object = reverse_param["expected_address"]
-    for object in expected_object:
-        expected_name = expected_object[object]
-        expected_response = json_response["address"][object]
-        if expected_name == expected_response:
-            print("Result:", expected_response)
-        assert expected_name == expected_response
-
-
-
+    with allure.step("Объявляем переменные с заданными параметрами"):
+        lat = reverse_param["coordinats"]["lat"]
+        lon = reverse_param["coordinats"]["lon"]
+        with allure.step("Создаем словарь с параметрами для отправки запроса)"):
+            dict_requests = {"lat": lat, "lon": lon, "format": "json", "accept-language": "ru"}
+            with allure.step("Отправляем запрос"):
+                response = requests.get("https://nominatim.openstreetmap.org/reverse?", params=dict_requests)
+                json_response = response.json()
+                expected_object = reverse_param["expected_address"]
+                with allure.step("Создаем фцикл по объектам заданных параметров"):
+                    for object in expected_object:
+                        expected_name = expected_object[object]
+                        expected_response = json_response["address"][object]
+                        with allure.step("Сравниваем результаты"):
+                            if expected_name == expected_response:
+                                print("Result:", expected_response)
+                                assert expected_name == expected_response
