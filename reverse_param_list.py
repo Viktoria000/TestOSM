61929350258abbb9b3116c369fbe59f8cbdb91be
Селькова <selkova@protei.ru>
2021-08-13 18:56:31 +0300

    Доработки

--- reverse_param_list.py
+++ reverse_param_list.py
@@ -5,12 +5,34 @@ reverse_param_list_value = [
             "city": "Москва",
             "country": "Россия"
         }
+
     },
     {
-        "coordinats": {"lat": 64.145981, "lon": -21.9422367},
+        "coordinats": {"lat": 1, "lon": 1},
         "expected_address": {
-            "city": "Рейкьявик",
-            "country": "Исландия"
+            "city": "Стамбул",
+            "country": "Турция"
+        }
+    },
+    {
+        "coordinats": {"lat": 53.902334, "lon": 27.5618791},
+        "expected_address": {
+            "city": "Минск",
+            "country": "Беларусь"
+        }
+    },
+    {
+        "coordinats": {"lat": 0000, "lon": 0000},
+        "expected_address": {
+            "city": "Москва",
+            "country": "Россия"
+        }
+    },
+    {
+        "coordinats": {"lat": 51.6605982, "lon": 39.2005858},
+        "expected_address": {
+            "city": "Воронеж",
+            "country": "Эфиопия"
         }
     }
 ]
--- test_search.py
+++ test_search.py
@@ -45,33 +45,37 @@ class TestOSM:
         return result
 
     # объявляем тест с параметрами (два набора входных данных)
-    @pytest.mark.parametrize("param_list", direct_param_list_value)
-    def test_direct_any_params(self, param_list):
-        req_params = param_list["req_params"]
-        coordinates_list = self.direct_search(req_params)
-        expected_lat = param_list["expected_lat"]
-        expected_lon = param_list["expected_lon"]
-        find_coordinates = False
+    # @pytest.mark.parametrize("param_list", direct_param_list_value)
+    # def test_direct_any_params(self, param_list):
+    #     req_params = param_list["req_params"]
+    #     coordinates_list = self.direct_search(req_params)
+    #     expected_lat = param_list["expected_lat"]
+    #     expected_lon = param_list["expected_lon"]
+    #     find_coordinates = False
+    #
+    #     for coordinates in coordinates_list:
+    #         if coordinates.lat == expected_lat and coordinates.lon == expected_lon:
+    #             find_coordinates = True
+    #             print("FOUND!!")
+    #             coordinates.print()
+    #     assert find_coordinates, "Не найдены ожидаемые координаты"
 
-        for coordinates in coordinates_list:
-            if coordinates.lat == expected_lat and coordinates.lon == expected_lon:
-                find_coordinates = True
-                print("FOUND!!")
-                coordinates.print()
-        assert find_coordinates, "Не найдены ожидаемые координаты"
-
-    # @pytest.mark.parametrize("reverse_param", reverse_param_list_value)
-    # def test_revers_param(self, reverse_param):
-    #     lat = reverse_param["coordinats"]["lat"]
-    #     lon = reverse_param["coordinats"]["lon"]
-    #     dict_requests = {"lat": lat, "lon": lon, "format": "json", "accept-language": "ru"}
-    #     response = requests.get("https://nominatim.openstreetmap.org/reverse?", params=dict_requests)
-    #     json_response = response.json()
-    #     expected_object = reverse_param["expected_address"]
-    #     for object in expected_object:
-    #         expected_name = expected_object[object]
-    #         expected_response = json_response["address"][object]
-    #         with allure.step("Сравниваем результаты"):
-    #             if expected_name == expected_response:
-    #                 print("Result:", expected_response)
-    #             assert expected_name == expected_response
+    @pytest.mark.parametrize("reverse_param", reverse_param_list_value)
+    def test_revers_param(self, reverse_param):
+        lat = reverse_param["coordinats"]["lat"]
+        lon = reverse_param["coordinats"]["lon"]
+        dict_requests = {"lat": lat, "lon": lon, "format": "json", "accept-language": "ru"}
+        response = requests.get("https://nominatim.openstreetmap.org/reverse?", params=dict_requests)
+        print("Response_URL:", response.url)
+        assert response.content, "Пустой ответ"
+        json_response = response.json()
+        assert json_response, "Пустой json"
+        assert "address" in json_response, "Пустой ответ"
+        expected_object = reverse_param["expected_address"]
+        for object in expected_object:
+            assert object in json_response["address"], "В ответе отсутствует указанный параметр"
+            expected_name = expected_object[object]
+            expected_response = json_response["address"][object]
+            if expected_name == expected_response:
+                print("Result:", object, expected_response)
+            assert expected_name == expected_response, "По данным координатам результат параметров не совпадает"
