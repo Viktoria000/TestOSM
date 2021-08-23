df8f8ffcd938bccc0b676d099dc44cc4c8afc453
Селькова <selkova@protei.ru>
2021-08-23 18:29:39 +0300

    Добавлены отчеты

--- test_search.py
+++ test_search.py
@@ -1,7 +1,6 @@
 import requests
 import pytest
 import allure
-import logging
 from direct_param_list import direct_param_list_value
 from reverse_param_list import reverse_param_list_value
 
@@ -21,16 +20,13 @@ class Coordinates:
         print("lon: ", self.lon)
         print("")
 
-class Server:
-    @pytest.fixture()
-    def server(self, response):
-        response = response.status_code
-        assert response == 200, "Не удалось отправить запрос"
-       response = response.content
-        assert response, "Пустой ответ"
-        response = response.json()
-        assert response, "Пустой json"
-
+# class Server:
+#     def __init__(self, url, params):
+#         self.url = url
+#         self.params = params
+#         response = requests.get(url, params)
+#         print(response.url)
+#
 
 
 class TestOSM:
@@ -38,12 +34,14 @@ class TestOSM:
     # объявляем функцию, добавляем параметр в словарь, отправляем запрос, ответ в формате джейсон
     def direct_search(self, req_params):
         req_params["format"] = "json"
-        response = requests.get("https://nominatim.openstreetmap.org/search?", params=req_params)
+        with allure.step("Запрос отправлен, получаем ответ"):
+            response = requests.get("https://nominatim.openstreetmap.org/search?", params=req_params)
         assert response.status_code == 200, "Не удалось отправить запрос"
         print("request URL:", response.url)
         print("response :", response.status_code)
         assert response.content, "Пустой ответ"
-        json_response = response.json()
+        with allure.step("Формируем ответ в формат json"):
+            json_response = response.json()
         print(json_response)
         assert json_response, "Пустой json"
         result = []
@@ -56,6 +54,7 @@ class TestOSM:
 
         return result
 
+    @allure.feature('Прямой запрос')
     @pytest.mark.parametrize("param_list", direct_param_list_value)
     def test_direct_any_params(self, param_list):
         req_params = param_list["req_params"]
@@ -63,29 +62,34 @@ class TestOSM:
         expected_lat = param_list["expected_lat"]
         expected_lon = param_list["expected_lon"]
         find_coordinates = False
-
-        for coordinates in coordinates_list:
-            if coordinates.lat == expected_lat and coordinates.lon == expected_lon:
-                find_coordinates = True
-                coordinates.print()
+        with allure.step("Сравниваем полученные результаты"):
+            for coordinates in coordinates_list:
+                if coordinates.lat == expected_lat and coordinates.lon == expected_lon:
+                    find_coordinates = True
+                    coordinates.print()
         assert find_coordinates, "Не найдены ожидаемые координаты"
 
+    @allure.feature("Обратный запрос")
     @pytest.mark.parametrize("reverse_param", reverse_param_list_value)
     def test_revers_param(self, reverse_param):
         lat = reverse_param["coordinats"]["lat"]
         lon = reverse_param["coordinats"]["lon"]
         dict_requests = {"lat": lat, "lon": lon, "format": "json", "accept-language": "ru"}
-        response = requests.get("https://nominatim.openstreetmap.org/reverse?", params=dict_requests)
+        with allure.step("Отправляем запрос и получаем ответ"):
+            response = requests.get("https://nominatim.openstreetmap.org/reverse?", params=dict_requests)
         print("Response_URL:", response.url)
         assert response.content, "Пустой ответ"
-        json_response = response.json()
+        with allure.step("Формируем ответ в формат json"):
+            json_response = response.json()
         assert json_response, "Пустой json"
         assert "address" in json_response, "Пустой ответ"
         expected_object = reverse_param["expected_address"]
-        for object in expected_object:
-            assert object in json_response["address"], "В ответе отсутствует указанный параметр"
+        with allure.step("Проверяем есть ли нужный параметр в ответе"):
+            for object in expected_object:
+                assert object in json_response["address"], "В ответе отсутствует указанный параметр"
             expected_name = expected_object[object]
             expected_response = json_response["address"][object]
-            if expected_name == expected_response:
-                print("Result:", object, expected_response)
+            with allure.step("Сравниваем результаты"):
+                if expected_name == expected_response:
+                    print("Result:", object, expected_response)
             assert expected_name == expected_response, "По данным координатам результат параметров не совпадает"
